e8d495c854cb9c94eb137d07f80dabeb300d4d43
denis-ok <strelkov.d.d@mail.ru>
2018-05-18 16:23:30 +0300

    add bootstrap (via webpack)

---
+++ client/index.js
@@ -0,0 +1,3 @@
+import 'bootstrap';
+import 'jquery-ujs';
+import './scss/app.scss';
---
+++ webpack.config.babel.js
@@ -0,0 +1,69 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import webpack from 'webpack';
+import autoprefixer from 'autoprefixer';
+import precss from 'precss';
+
+dotenv.config();
+
+export default () => ({
+  entry: {
+    app: ['./client'],
+  },
+  output: {
+    path: path.join(__dirname, 'public', 'assets'),
+    filename: 'app.js',
+    // publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [precss, autoprefixer],
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+  ],
+  optimization: {
+    // minimize: false,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  mode: process.env.NODE_ENV || 'production',
+});
+
