fc2eaa661782188b6f8ffcdef542d941a80d1d28
denis-ok <strelkov.d.d@mail.ru>
2018-05-26 22:24:18 +0300

    update model(watermeter) add helper methods

--- src/models/waterMeter.js
+++ src/models/waterMeter.js
@@ -1,3 +1,62 @@
+import _ from 'lodash';
+
+const getFirstReadoutValue = async (watermeter) => {
+  const readouts = await watermeter.getReadouts({
+    limit: 1,
+    order: [['date', 'ASC']],
+  });
+
+  const readout = readouts[0];
+  const { value } = readout;
+  return value;
+};
+
+const getLastReadoutValue = async (watermeter) => {
+  const readouts = await watermeter.getReadouts({
+    limit: 1,
+    order: [['date', 'DESC']],
+  });
+
+  const readout = readouts[0];
+  const { value } = readout;
+  return value;
+};
+
+const getTotalConsumption = async (watermeter) => {
+  const first = await getFirstReadoutValue(watermeter);
+  const last = await getLastReadoutValue(watermeter);
+
+  const consumption = last - first;
+  return consumption;
+};
+
+const getReadoutsCount = async (watermeter) => {
+  const readouts = await watermeter.getReadouts();
+  return readouts.length;
+};
+
+const getPeriodsCount = async (watermeter) => {
+  const readouts = await watermeter.getReadouts();
+  const readoutsCount = readouts.length
+  return readoutsCount <= 1 ? 0 : readoutsCount - 1;
+};
+
+
+const getAverageMonthlyConsumption = async (watermeter) => {
+  const readouts = await watermeter.getReadouts();
+
+  if (readouts.length <= 1) {
+    return 0;
+  }
+
+  const periods = readouts.length - 1;
+
+  const totalConsumption = await getTotalConsumption(watermeter);
+  const result = totalConsumption / periods;
+
+  return _.round(result, 3);
+};
+
 export default (sequelize, DataTypes) => {
   const WaterMeter = sequelize.define('WaterMeter', {
     waterType: DataTypes.STRING,
@@ -11,6 +70,36 @@ export default (sequelize, DataTypes) => {
     const hotWm = WaterMeter.create({ waterType: 'hot' });
     return [coldWm, hotWm];
   };
+
+  WaterMeter.prototype.getReadoutsCount = function() {
+    const watermeter = this;
+    return getReadoutsCount(watermeter);
+  };
+
+  WaterMeter.prototype.getPeriodsCount = function() {
+    const watermeter = this;
+    return getPeriodsCount(watermeter);
+  };
+
+  WaterMeter.prototype.getFirstReadoutValue = function() {
+    const watermeter = this;
+    return getFirstReadoutValue(watermeter);
+  };
+
+  WaterMeter.prototype.getLastReadoutValue = function() {
+    const watermeter = this;
+    return getLastReadoutValue(watermeter);
+  };
+
+  WaterMeter.prototype.getTotalConsumption = function() {
+    const watermeter = this;
+    return getTotalConsumption(watermeter);
+  };
+
+  WaterMeter.prototype.getAverageMonthlyConsumption = function() {
+    const watermeter = this;
+    return getAverageMonthlyConsumption(watermeter);
+  };  
 
   return WaterMeter;
 };
